c4b63113d7efa45ae00dd70f0826769607f1d458
Kurt Wilson <kurt4wilson@gmail.com>
2019-01-30 22:09:25 -0500

    quick checkpoint

--- app/scoutserver/src/components/App.js
+++ app/scoutserver/src/components/App.js
@@ -2,9 +2,23 @@ import React from "react";
 import ReactDOM from "react-dom";
 import DataProvider from "./DataProvider";
 import Table from "./Table";
+import SignIn from "./Signin";
+import SimpleAppBar from "./TopAppBar"
+import Drawer from '@material-ui/core/Drawer';
 const App = () => (
-    <DataProvider endpoint="api/matches/"
-        render={data => <Table data={data} />} />
+    <div>
+        <div>
+            <SimpleAppBar />
+        </div>
+        <div>
+            <Drawer />
+        </div>
+        <div>
+            <SignIn />
+        </div>
+    </div>
+    // <DataProvider endpoint="api/matches/"
+    //     render={data => <Table data={data} />} />
 );
 const wrapper = document.getElementById("app");
 wrapper ? ReactDOM.render(<App />, wrapper) : null;
