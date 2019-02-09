241250b6ce736215f98d71ee744c4bef0f17bef3
Kurt Wilson <kurt4wilson@gmail.com>
2019-02-08 21:37:25 -0500

    actually add django_heroku correctly this time

--- app/swampscout/settings.py
+++ app/swampscout/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 """
 
 import os
-import django-heroku
+import django_heroku
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -134,3 +134,5 @@ REST_FRAMEWORK = {
         'rest_framework.authentication.TokenAuthentication',
     )
 }
+
+django_heroku.settings(locals())
