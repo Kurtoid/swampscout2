a0183a29d568f2a71579a8012073d7059a0c095e
Kurt Wilson <kurt4wilson@gmail.com>
2019-02-13 21:03:59 -0500

    add whitenoise?

--- app/swampscout/settings.py
+++ app/swampscout/settings.py
@@ -51,6 +51,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware', # add this line
 ]
 
 ROOT_URLCONF = 'swampscout.urls'
