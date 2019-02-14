82dab9b2958befa5b8e93f42ff0da92368b78712
Kurt Wilson <kurt4wilson@gmail.com>
2019-02-13 21:05:36 -0500

    another tweak

--- app/swampscout/settings.py
+++ app/swampscout/settings.py
@@ -123,7 +123,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = 'staticfiles'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 AUTH_USER_MODEL = "scoutserver.MyUser"
 REST_FRAMEWORK = {
     # Use Django's standard `django.contrib.auth` permissions,
