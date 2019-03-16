62b0aa2b2fc5594f2227b0c07fa16f879d35e094
Kurt Wilson <kurt4wilson@gmail.com>
2019-03-15 23:19:19 -0400

    make precache match manifest start

--- app/scoutserver/templates/scoutserver/sw.js
+++ app/scoutserver/templates/scoutserver/sw.js
@@ -19,8 +19,8 @@ const RUNTIME = 'runtime';
 
 // A list of local resources we always want to be cached.
 const PRECACHE_URLS = [
-   './', // Alias for index.html
-    './static/scoutserver/main.js'
+   '/', 
+    '/static/scoutserver/main.js'
 ];
 
 // The install handler takes care of precaching the resources we always need.
